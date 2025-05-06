cd1b9784d2d66d18a09d05d33a0ca945a63a226b
Devilkd23 <148605780+Devilkd23@users.noreply.github.com>
2025-05-06 11:32:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
+
 @app.route('/')
-def index():    
-    return render_template('index.html')
+def home():
+    return "Hello, Google App Engine!"
+
+if __name__ == '__main__':
+    app.run()
